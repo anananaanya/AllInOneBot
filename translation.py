83f421f28e53d4afa2c661e15ff25e159c55a996
anananaanya <70605824+anananaanya@users.noreply.github.com>
2020-09-02 04:33:10 +0530

    Create translation.py

---
+++ translation.py
@@ -0,0 +1,81 @@
+class Translation(object):
+    START_TEXT = """This is an Mutli Purpose Bot
+    
+    
+     ┈┈┈••💙✿❤️✿💚••┈┈┈
+AnyDLclone bot created by @shreevish
+
+For more updates☛ @myTestBotz"""
+    RENAME_403_ERR = "Sorry. You Are Not Permitted To Rename This File."
+    ABS_TEXT = " Please Don't Be Selfish."
+    UPGRADE_TEXT = "Contact @shreevish"
+    FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
+    SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
+URL | filename | username | password"""
+    NOYES_URL = "@robot URL detected. Please use https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
+    DOWNLOAD_START = "trying to download"
+    UPLOAD_START = "trying to upload"
+    RCHD_BOT_API_LIMIT = "size greater than maximum allowed size (50MB). Neverthless, trying to upload."
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload Files Greater Than 2.0 GB Due to Telegram API limitations."
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "Follow Our Bots @HB4All_Bot"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \nUploaded in {} seconds."
+    NOT_AUTH_USER_TEXT = "@HB4All_Bot"
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users can only upload: {}\nIf you Think This is a bug, please contact <a href='https://t.me/shreevish?start=true'>@shreevish</a>"
+    SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in the video / file."
+    DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully."
+    SAVED_RECVD_DOC_FILE = "Document Downloaded Successfully."
+    CUSTOM_CAPTION_UL_FILE = " "
+    NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found."
+    NO_VOID_FORMAT_FOUND = "Select Below Format \n<b>YouTubeDL</b> said: {}"
+    USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
+    CURENT_PLAN_DETAILS = """Current plan details
+--------
+Telegram ID: <code>{}</code>
+Plan name: No Plan
+Expires on: {}"""
+    HELP_USER = """There Are Multiple Things I can do:
+👉 Download Youtube Videos And All Direct Link Also With Custom File Name ( Link | Filename.NameOfExtension )
+👉 Upload As File From Any HTTP link, With Custom Thumbnail Support( Link | Filename.NameOfExtension )
+👉 Convert To Streamable Video, Any Telegram File To Streamble Video Reply To File As /converttovideo
+👉 Convert To Telegram Audio, The media Sent As Telegram Documents Reply To File As /converttoaudio
+👉 Rename Telegram files, with Custom Thumbnail Support Reply To File As /Rename FileName.Nameextention
+👉 Trim Large Videos Reply To File As /Trim hh:mm:ss hh:mm:ss , And Take Screenshotsof Telegram Media Files Reply To File As /Trim hh:mm:ss 
+👉 Extract Compressed Telegram Media Reply To File As /unzip Reply To File</a>
+👉 Get A Telegram Sticker As Telegram Downloadable Media Simple Send Sticker It Will Generate
+--------
+Send /me to know current plan details"""
+    REPLY_TO_DOC_GET_LINK = "Reply to a Telegram media to get High Speed Direct Download Link"
+    REPLY_TO_DOC_FOR_C2V = "Reply to a Telegram media to convert"
+    REPLY_TO_DOC_FOR_SCSS = "Reply to a Telegram media to get screenshots"
+    REPLY_TO_DOC_FOR_RENAME_FILE = "Reply to a Telegram media to /rename with custom thumbnail support"
+    AFTER_GET_DL_LINK = "Direct Link <a href='{}'>Generated</a> valid for {} days."
+    FF_MPEG_RO_BOT_RE_SURRECT_ED = """Syntax: /trim HH:MM:SS [HH:MM:SS]"""
+    FF_MPEG_RO_BOT_STEP_TWO_TO_ONE = "First send /downloadmedia to any media so that it can be downloaded to my local. \nSend /storageinfo to know the media, that is currently downloaded."
+    FF_MPEG_RO_BOT_STOR_AGE_INFO = "Video Duration: {}\nSend /clearffmpegmedia to delete this media, from my storage.\nSend /trim HH:MM:SS [HH:MM:SS] to cu[l]t a small photo / video, from the above media."
+    FF_MPEG_RO_BOT_STOR_AGE_ALREADY_EXISTS = "A saved media already exists. Please send /storageinfo to know the current media details."
+    USER_DELETED_FROM_DB = "User <a href='tg://user?id={}'>{}</a> deleted from DataBase."
+    REPLY_TO_DOC_OR_LINK_FOR_RARX_SRT = "Reply to a Telegram media (MKV), to extract embedded streams"
+    REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB = "Reply /generatecustomthumbnail to a media album, to generate custom thumbail"
+    ERR_ONLY_TWO_MEDIA_IN_ALBUM = "Media Album should contain only two photos. Please re-send the media album, and then try again, or send only two photos in an album."
+    INVALID_UPLOAD_BOT_URL_FORMAT = "URL format is incorrect. make sure your url starts with either http:// or https://. You can set custom file name using the format link | file_name.extension"
+    ABUSIVE_USERS = "You are not allowed to use this bot. If you think this is a mistake, please check /me to remove this restriction."
+    FF_MPEG_RO_BOT_AD_VER_TISE_MENT = "https://telegram.dog/FFMpegRoBot"
+    EXTRACT_ZIP_INTRO_ONE = "Send a compressed file first, Then reply /unzip command to the file."
+    EXTRACT_ZIP_INTRO_THREE = "Analyzing received file. ⚠️ This might take some time. Please be patient. "
+    UNZIP_SUPPORTED_EXTENSIONS = ("zip", "rar")
+    EXTRACT_ZIP_ERRS_OCCURED = "Sorry. Errors occurred while processing compressed file. Please check everything again twice, and if the issue persists, Report this to <a href='https://t.me/shreevish?start=true'>@shreevish</a>"
+    EXTRACT_ZIP_STEP_TWO = """Select file_name to upload from the below options.
+You can use /rename command after receiving file to rename it with custom thumbnail support."""
+    CANCEL_STR = "Process Cancelled"
+    ZIP_UPLOADED_STR = "Uploaded {} files in {} seconds"
+    FREE_USER_LIMIT_Q_SZE = """Cannot Process.
+Free users only 1 request per 30 minutes.
+/upgrade or Try 1800 seconds later."""
+    SLOW_URL_DECED = "Gosh that Seems To BeA Very Slow URL. Since You Were Screwing My Home, I Am in No Mood To Download This File."
+    IFLONG_FILE_NAME = """File Name limit allowed by Telegram is {alimit} characters.
+The given file name has {num} characters.
+
+<b>Essays Not allowed in Telegram file name!</b>
+©️ <code>@shreevish</code>
+Please short your file name and try again!"""
